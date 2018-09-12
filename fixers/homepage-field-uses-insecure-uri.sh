d34c7c714e8f3952917161a215a5e45a87a1c5c2
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-09-12 17:35:14 +0100

    Fix a few more homepage URLs.

--- fixers/homepage-field-uses-insecure-uri.sh
+++ fixers/homepage-field-uses-insecure-uri.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 perl -p -i -e 's/^Homepage: http:\/\/github.com/Homepage: https:\/\/github.com/' debian/control
+perl -p -i -e 's/^Homepage: http:\/\/launchpad.net/Homepage: https:\/\/launchpad.net/' debian/control
+perl -p -i -e 's/^Homepage: http:\/\/pypi.python.org/Homepage: https:\/\/pypi.python.org/' debian/control
+perl -p -i -e 's/^Homepage: http:\/\/pear.php.net/Homepage: https:\/\/pear.php.net/' debian/control
+perl -p -i -e 's/^Homepage: http:\/\/pecl.php.net/Homepage: https:\/\/pecl.php.net/' debian/control
 echo "Use secure URI in Homepage field."
--- fixers/vcs-field-uses-insecure-uri.sh
+++ fixers/vcs-field-uses-insecure-uri.sh
@@ -5,4 +5,5 @@ perl -p -i -e 's/^Vcs-Git: http:\/\/salsa.debian.org/Vcs-Git: https:\/\/salsa.de
 perl -p -i -e 's/^Vcs-Browser: http:\/\/salsa.debian.org/Vcs-Browser: https:\/\/salsa.debian.org/' debian/control
 perl -p -i -e 's/^Vcs-Browser: git:\/\/github.com/Vcs-Browser: https:\/\/github.com/' debian/control
 perl -p -i -e 's/^Vcs-Browser: http:\/\/github.com/Vcs-Browser: https:\/\/github.com/' debian/control
+perl -p -i -e 's/^Vcs-Git: git:\/\/git.launchpad.net/Vcs-Git: https:\/\/git.launchpad.net/' debian/control
 echo "Use secure URI in Vcs control header."
