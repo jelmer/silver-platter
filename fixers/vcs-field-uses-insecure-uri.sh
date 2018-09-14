5d1b293d1e1dbb2ff67a9693dcaac34aec9e5558
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-09-14 01:55:41 +0100

    Support lp: URLs in vcs-field-uses-insecure-uri.

--- fixers/vcs-field-uses-insecure-uri.sh
+++ fixers/vcs-field-uses-insecure-uri.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo "Use secure URI in Vcs control header."
 perl -p -i -e 's/^Vcs-Git: git:\/\/github.com/Vcs-Git: https:\/\/github.com/' debian/control
 perl -p -i -e 's/^Vcs-Git: http:\/\/github.com/Vcs-Git: https:\/\/github.com/' debian/control
 perl -p -i -e 's/^Vcs-Git: http:\/\/salsa.debian.org/Vcs-Git: https:\/\/salsa.debian.org/' debian/control
@@ -6,4 +7,9 @@ perl -p -i -e 's/^Vcs-Browser: http:\/\/salsa.debian.org/Vcs-Browser: https:\/\/
 perl -p -i -e 's/^Vcs-Browser: git:\/\/github.com/Vcs-Browser: https:\/\/github.com/' debian/control
 perl -p -i -e 's/^Vcs-Browser: http:\/\/github.com/Vcs-Browser: https:\/\/github.com/' debian/control
 perl -p -i -e 's/^Vcs-Git: git:\/\/git.launchpad.net/Vcs-Git: https:\/\/git.launchpad.net/' debian/control
-echo "Use secure URI in Vcs control header."
+if grep "Vcs-Bzr: lp:" debian/control >/dev/null; then
+  echo
+  echo "The lp: prefix gets expanded to http://code.launchpad.net/ for "
+  echo "users that are not logged in on some versions of Bazaar."
+fi
+perl -p -i -e 's/^Vcs-Bzr: lp:/Vcs-Bzr: https:\/\/code.launchpad.net\//' debian/control
