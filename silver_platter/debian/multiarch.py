c2df3233733a76f4222d61bb5e160b1ca1620acd
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-03-26 04:02:21 +0000

    Fix apply-multi-arch-hints command.

--- silver_platter/debian/multiarch.py
+++ silver_platter/debian/multiarch.py
@@ -28,6 +28,8 @@ from .changer import (
     setup_parser,
     )
 
+from breezy.trace import note
+
 BRANCH_NAME = 'multi-arch-fixes'
 
 
@@ -37,13 +39,22 @@ class MultiArchHintsChanger(DebianChanger):
 
     @classmethod
     def setup_parser(cls, parser):
-        pass
+        # Hide the minimum-certainty option for the moment.
+        parser.add_argument(
+            '--minimum-certainty',
+            type=str,
+            choices=SUPPORTED_CERTAINTIES,
+            default=None,
+            help=argparse.SUPPRESS)
+        parser.add_argument(
+            '--allow-reformatting', default=None, action='store_true',
+            help=argparse.SUPPRESS)
 
     @classmethod
     def from_args(cls, args):
-        return cls()
+        return cls(args.minimum_certainty, args.allow_reformatting)
 
-    def __init__(self):
+    def __init__(self, minimum_certainty=None, allow_reformatting=None):
         from lintian_brush.multiarch_hints import (
             download_multiarch_hints,
             multiarch_hints_by_binary,
@@ -51,6 +62,8 @@ class MultiArchHintsChanger(DebianChanger):
             )
         with download_multiarch_hints() as f:
             self.hints = multiarch_hints_by_binary(parse_multiarch_hints(f))
+        self.minimum_certainty = minimum_certainty
+        self.allow_reformatting = allow_reformatting
 
     def suggest_branch_name(self):
         return BRANCH_NAME
@@ -59,14 +72,17 @@ class MultiArchHintsChanger(DebianChanger):
         from lintian_brush.multiarch_hints import (
             MultiArchHintFixer,
             )
+        minimum_certainty = self.minimum_certainty
+        allow_reformatting = self.allow_reformatting
         try:
             cfg = Config.from_workingtree(local_tree, subpath)
         except FileNotFoundError:
-            minimum_certainty = None
-            allow_reformatting = False
+            pass
         else:
-            minimum_certainty = cfg.minimum_certainty()
-            allow_reformatting = cfg.allow_reformatting()
+            if minimum_certainty is None:
+                minimum_certainty = cfg.minimum_certainty()
+            if allow_reformatting is None:
+                allow_reformatting = cfg.allow_reformatting()
             if update_changelog is None:
                 update_changelog = cfg.update_changelog()
 
@@ -77,10 +93,12 @@ class MultiArchHintsChanger(DebianChanger):
             subpath=subpath, allow_reformatting=allow_reformatting,
             net_access=True)
 
+        return result
+
     def get_proposal_description(
             self, applied, description_format, existing_proposal):
         ret = ['Apply multi-arch hints.\n']
-        for binary, description, certainty in applied.changes:
+        for binary, hint, description, certainty in applied.changes:
             ret.append('* %s: %s\n' % (binary['Package'], description))
         return ''.join(ret)
 
@@ -91,7 +109,9 @@ class MultiArchHintsChanger(DebianChanger):
         return True
 
     def describe(self, applied, publish_result):
-        raise NotImplementedError(self.describe)
+        note('Applied multi-arch hints.')
+        for binary, hint, description, certainty in applied.changes:
+            note('* %s: %s', binary['Package'], description)
 
     def tags(self, applied):
         return []
