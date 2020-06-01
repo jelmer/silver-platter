865bab52613c2b5a010349849217530066f9fe58
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-06-01 21:43:24 +0000

    Import hoster-specific plugins.

--- silver_platter/proposal.py
+++ silver_platter/proposal.py
@@ -50,7 +50,12 @@ from breezy.propose import (
     HosterLoginRequired,
     )
 
-import breezy.plugins.propose  # noqa: F401
+try:
+    import breezy.plugins.gitlab  # noqa: F401
+    import breezy.plugins.github  # noqa: F401
+    import breezy.plugins.launchpad  # noqa: F401
+except ImportError:  # breezy < 3.1.0
+    import breezy.plugins.propose  # noqa: F401
 
 from .utils import (
     create_temp_sprout,
