8df6ed98f1f458972e8eaa5b9ef1df6ea7d0b436
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-07-26 17:35:11 +0000

    Fix typo.

--- silver_platter/proposal.py
+++ silver_platter/proposal.py
@@ -677,7 +677,7 @@ def propose_changes(
             try:
                 mp = proposal_builder.create_proposal(
                     description=mp_description, labels=labels,
-                    reviewers=reviewers, owners=owner, **kwargs)
+                    reviewers=reviewers, owner=owner, **kwargs)
             except PermissionDenied:
                 note('Permission denied while trying to create '
                      'proposal.')
