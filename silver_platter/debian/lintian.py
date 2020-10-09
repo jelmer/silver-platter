fa9fea89b4b157354bdc59696c897698d2d98ae2
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-10-09 00:07:30 +0000

    Add missing import.

--- silver_platter/debian/lintian.py
+++ silver_platter/debian/lintian.py
@@ -16,6 +16,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 import argparse
+import sys
 from typing import List, Set
 
 from debian.changelog import ChangelogCreateError
