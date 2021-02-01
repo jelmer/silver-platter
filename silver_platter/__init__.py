2eefbc44b30eed03db8f0977b6099873fadaebb3
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-02-01 23:30:14 +0000

    Release 0.4.0.

--- silver_platter/__init__.py
+++ silver_platter/__init__.py
@@ -25,5 +25,5 @@ import breezy.plugins.gitlab  # For gitlab support  # noqa: F401
 import breezy.plugins.github  # For github support  # noqa: F401
 import breezy.plugins.debian  # For apt: URL support  # noqa: F401
 
-__version__ = (0, 3, 0)
+__version__ = (0, 4, 0)
 version_string = '.'.join(map(str, __version__))
