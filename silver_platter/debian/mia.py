00f628b8895a9eb2f7565adfc6058ebb5d42f1e6
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-05-02 16:28:55 +0100

    Split out tree_drop_mia_maintainers.

--- silver_platter/debian/mia.py
+++ silver_platter/debian/mia.py
@@ -91,6 +91,53 @@ def drop_uploaders(editor, mia_people):
     return removed_mia
 
 
+def tree_drop_mia_uploaders(local_tree, subpath, update_changelog=None, committer=None):
+    control_path = local_tree.abspath(osutils.pathjoin(subpath, "debian/control"))
+    changelog_entries = []
+    with ControlEditor(path=control_path) as editor:
+        source = editor.source["Source"]
+        bugs = all_mia_teammaint_bugs().intersection(get_package_bugs(source))
+        if not bugs:
+            raise ChangerError("nothing-to-do", "No MIA people")
+        removed_uploaders = []
+        fixed_bugs = []
+        for bug in bugs:
+            mia_people = get_mia_maintainers(bug)
+
+            if mia_people is None:
+                logging.warning('No MIA people (X-Debbugs-CC) found in bug %d', bug)
+                continue
+
+            removed_mia = drop_uploaders(editor, mia_people)
+
+            if len(removed_mia) == 0:
+                continue
+
+            if len(removed_mia) == 1:
+                description = "Remove MIA uploader %s." % removed_mia[0]
+            else:
+                description = "Remove MIA uploaders %s." % (
+                    ", ".join(removed_mia)
+                )
+            if removed_mia == mia_people:
+                description += " Closes: #%d" % bug
+            changelog_entries.append(description)
+            removed_uploaders.extend(removed_mia)
+
+    if not changelog_entries:
+        return MIAResult(source, removed_uploaders=[], bugs=fixed_bugs)
+    if update_changelog in (True, None):
+        add_changelog_entry(
+            local_tree,
+            osutils.pathjoin(subpath, "debian/changelog"),
+            changelog_entries,
+        )
+    local_tree.commit(
+        "Remove MIA uploaders.", committer=committer, allow_pointless=False
+    )
+    return MIAResult(source, removed_uploaders=removed_uploaders, bugs=fixed_bugs)
+
+
 class MIAChanger(DebianChanger):
 
     name = "mia"
@@ -119,56 +166,10 @@ class MIAChanger(DebianChanger):
         base_proposal=None,
     ):
         base_revid = local_tree.last_revision()
-        control_path = local_tree.abspath(osutils.pathjoin(subpath, "debian/control"))
         try:
-            changelog_entries = []
-            with ControlEditor(path=control_path) as editor:
-                source = editor.source["Source"]
-                bugs = all_mia_teammaint_bugs().intersection(get_package_bugs(source))
-                if not bugs:
-                    raise ChangerError("nothing-to-do", "No MIA people")
-                removed_uploaders = []
-                fixed_bugs = []
-                for bug in bugs:
-                    mia_people = get_mia_maintainers(bug)
-
-                    if mia_people is None:
-                        logging.warning('No MIA people (X-Debbugs-CC) found in bug %d', bug)
-                        continue
-
-                    removed_mia = drop_uploaders(editor, mia_people)
-
-                    if len(removed_mia) == 0:
-                        continue
-
-                    if len(removed_mia) == 1:
-                        description = "Remove MIA uploader %s." % removed_mia[0]
-                    else:
-                        description = "Remove MIA uploaders %s." % (
-                            ", ".join(removed_mia)
-                        )
-                    if removed_mia == mia_people:
-                        description += " Closes: #%d" % bug
-                    changelog_entries.append(description)
-                    removed_uploaders.extend(removed_mia)
-
-            result = MIAResult(source, removed_uploaders=removed_uploaders, bugs=fixed_bugs)
-            reporter.report_metadata("bugs", fixed_bugs)
-            reporter.report_metadata("removed_uploaders", removed_uploaders)
-
-            if not changelog_entries:
-                raise ChangerError(
-                    "nothing-to-do", "Unable to remove any MIA uploaders"
-                )
-            if update_changelog in (True, None):
-                add_changelog_entry(
-                    local_tree,
-                    osutils.pathjoin(subpath, "debian/changelog"),
-                    changelog_entries,
-                )
-            local_tree.commit(
-                "Remove MIA uploaders.", committer=committer, allow_pointless=False
-            )
+            result = tree_drop_mia_uploaders(
+                local_tree, subpath, update_changelog=update_changelog,
+                committer=committer)
         except FormattingUnpreservable as e:
             raise ChangerError(
                 "formatting-unpreservable",
@@ -177,6 +178,14 @@ class MIAChanger(DebianChanger):
         except (ChangeConflict, GeneratedFile) as e:
             raise ChangerError(
                 "generated-file", "unable to edit generated file: %r" % e
+            )
+
+        reporter.report_metadata("bugs", result.bugs)
+        reporter.report_metadata("removed_uploaders", result.removed_uploaders)
+
+        if not result.removed_uploaders:
+            raise ChangerError(
+                "nothing-to-do", "Unable to remove any MIA uploaders"
             )
 
         branches = [("main", None, base_revid, local_tree.last_revision())]
