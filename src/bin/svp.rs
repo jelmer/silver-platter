0132213ba50b228a68b6fd5b18616ed7c4dab3f4
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-12-30 17:47:33 +0000

    Migrate login to rust

--- src/bin/svp.rs
+++ src/bin/svp.rs
@@ -9,6 +9,7 @@ use silver_platter::publish::Error as PublishError;
 use silver_platter::CodemodResult;
 
 use silver_platter::Mode;
+use std::collections::HashMap;
 use std::io::Write;
 use std::path::Path;
 
@@ -411,6 +412,87 @@ pub fn batch_publish(
     }
 }
 
+fn login(url: &url::Url) -> i32 {
+    pyo3::Python::with_gil(|py| {
+        let m = py.import("launchpadlib").unwrap();
+        let lp_uris = match m.getattr("uris") {
+            Ok(lp_uris) => lp_uris
+                .getattr("web_roots")
+                .unwrap()
+                .extract::<HashMap<String, String>>()
+                .unwrap(),
+            Err(e) if e.is_instance_of::<pyo3::exceptions::PyModuleNotFoundError>(py) => {
+                log::warn!("launchpadlib is not installed, unable to log in to launchpad");
+                HashMap::new()
+            }
+            Err(e) => {
+                panic!("Failed to import launchpadlib: {}", e);
+            }
+        };
+
+        let forge = if url.host_str() == Some("github.com") {
+            "github"
+        } else if lp_uris.iter().any(|(_key, root)| {
+            url.host_str() == Some(root) || url.host_str() == Some(root.trim_end_matches('/'))
+        }) {
+            "launchpad"
+        } else {
+            "gitlab"
+        };
+
+        match forge {
+            "gitlab" => {
+                let m = py.import("breezy.plugins.gitlab.cmds").unwrap();
+                let cmd = m.getattr("cmd_gitlab_login").unwrap();
+
+                let cmd_gl = cmd.call0().unwrap();
+                cmd_gl.call_method0("_setup_outf").unwrap();
+
+                cmd_gl.call_method1("run", (url.as_str(),)).unwrap();
+            }
+            "github" => {
+                let m = py.import("breezy.plugins.github.cmds").unwrap();
+                let cmd = m.getattr("cmd_github_login").unwrap();
+
+                let cmd_gl = cmd.call0().unwrap();
+                cmd_gl.call_method0("_setup_outf").unwrap();
+
+                cmd_gl.call_method0("run").unwrap();
+            }
+            "launchpad" => {
+                let m = py.import("breezy.plugins.launchpad.cmds").unwrap();
+                let cmd = m.getattr("cmd_launchpad_login").unwrap();
+
+                let cmd_lp = cmd.call0().unwrap();
+                cmd_lp.call_method0("_setup_outf").unwrap();
+
+                cmd_lp.call_method1("run", (url.as_str(),)).unwrap();
+
+                let lp_api = py.import("breezy.plugins.launchpad.lp_api").unwrap();
+
+                let lp_service_root = lp_uris
+                    .iter()
+                    .find(|(_key, root)| {
+                        url.host_str() == Some(root)
+                            || url.host_str() == Some(root.trim_end_matches('/'))
+                    })
+                    .unwrap()
+                    .1;
+                let kwargs = pyo3::types::PyDict::new(py);
+                kwargs.set_item("version", "devel").unwrap();
+                lp_api
+                    .call_method("connect_launchpad", (lp_service_root,), Some(kwargs))
+                    .unwrap();
+            }
+            _ => {
+                panic!("Unknown forge {}", forge);
+            }
+        }
+
+        0
+    })
+}
+
 fn main() {
     pyo3::prepare_freethreaded_python();
     let cli = Cli::parse();
@@ -444,9 +526,7 @@ fn main() {
             }
             0
         }
-        Commands::Login { url: _ } => {
-            todo!();
-        }
+        Commands::Login { url } => login(url),
         Commands::Proposals { status } => {
             let statuses = status.as_ref().map(|status| vec![*status]);
             for (_forge, proposal) in silver_platter::proposal::iter_all_mps(statuses) {
