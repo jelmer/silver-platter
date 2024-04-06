8a115d1949863d5c27b99168ca0899f928c53c99
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-04-06 21:40:17 +0100

    Install binary from rust rather than python

--- setup.py
+++ setup.py
@@ -18,6 +18,5 @@ setup(
             args=["--no-default-features"],
             features=features + ["extension-module"],
         ),
-        RustBin("svp", "Cargo.toml", features=features + ["cli"]),
     ],
 )
